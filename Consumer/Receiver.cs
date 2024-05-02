a769e0728a7f8a9e4fb7802044dc4878c5b528f0
Yesenia López Giraldo <yesenialopezg07@gmail.com>
2024-05-02 14:37:33 -0500

    project

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "admin",
+    Password = "admin"
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("udemyQueue", false, false, false, null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.Span;
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine("Mensaje recibido {0}", message);
+    };
+
+    channel.BasicConsume("udemyQueue", true, consumer);
+
+    Console.WriteLine("Presiona [enter] para salir del consumer");
+    Console.ReadLine();
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "admin",
+    Password = "admin"
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel()) 
+{
+    channel.QueueDeclare("udemyQueue", false, false, false, null);
+
+    var message = "Bienvenido";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "udemyQueue", null, body);
+    Console.WriteLine("El mensaje fue enviado {0}", message);
+}
+
+Console.WriteLine("Presiona [Enter] para salir de la aplicación");
+Console.ReadLine();
